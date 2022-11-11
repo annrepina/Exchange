082f5fa8d6da7a035da2b5e55d33eb8d72db394e
a.repina <annetrepina@mail.ru>
2022-11-11 17:01:23 +0300

    Добавьте файлы проекта.

---
+++ Exchange/Model/Currency.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Model
+{
+    /// <summary>
+    /// Валюта
+    /// </summary>
+    public class Currency
+    {
+        /// <summary>
+        /// Имя валюты
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Код валюты
+        /// </summary>
+        public int CurrencyCode { get; set; }
+
+        /// <summary>
+        /// Количество валюты
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Цена валюты
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Currency()
+        {
+
+        }
+    }
+
+}
---
+++ Exchange/Model/CurrencyExchange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Model
+{
+    /// <summary>
+    /// Класс - валютная биржа
+    /// </summary>
+    public class CurrencyExchange
+    {
+        //public I
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public CurrencyExchange()
+        {
+
+        }
+    }
+}
